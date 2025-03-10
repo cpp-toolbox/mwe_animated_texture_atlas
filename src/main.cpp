109fc94f58eeae31cf39a4f3ed37147e0b320fdb
cuppajoeman <ccn@cuppajoeman.com>
2025-03-10 05:13:16 -0400

    feat: update infrastructure

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,26 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
 #include "graphics/animated_texture_atlas/animated_texture_atlas.hpp"
+#include "graphics/draw_info/draw_info.hpp"
+#include "graphics/vertex_geometry/vertex_geometry.hpp"
 #include "graphics/shader_standard/shader_standard.hpp"
-#include "graphics/window/window.hpp"
+#include "graphics/texture_packer/texture_packer.hpp"
 #include "graphics/shader_cache/shader_cache.hpp"
 #include "graphics/batcher/generated/batcher.hpp"
-#include "graphics/texture_packer/texture_packer.hpp"
 #include "graphics/fps_camera/fps_camera.hpp"
-#include "graphics/vertex_geometry/vertex_geometry.hpp"
+#include "graphics/window/window.hpp"
+
 #include "utility/glfw_lambda_callback_manager/glfw_lambda_callback_manager.hpp"
 #include "utility/texture_packer_model_loading/texture_packer_model_loading.hpp"
+#include "utility/glm_printing/glm_printing.hpp"
+#include "utility/print_utils/print_utils.hpp"
+#include "utility/unique_id_generator/unique_id_generator.hpp"
 
 #define STB_IMAGE_IMPLEMENTATION
-
+#define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <stb_image.h>
+#include <stb_image_write.h>
 
 #include <cstdio>
 #include <cstdlib>
@@ -50,22 +57,20 @@ int main() {
 
     std::vector<spdlog::sink_ptr> sinks = {console_sink, file_sink};
 
-    float flame_height = 4;
-    float flame_width = 1.7;
-    auto flame_vertices = generate_rectangle_vertices(0, 0, flame_width, flame_height);
-    auto flame_indices = generate_rectangle_indices();
-    Transform flame;
-    flame.position = glm::vec3(0, flame_height / 2.0, 0);
-
     GLFWwindow *window =
         initialize_glfw_glad_and_return_window(SCREEN_WIDTH, SCREEN_HEIGHT, "glfw window", false, true, false);
 
     std::vector<ShaderType> requested_shaders = {ShaderType::TEXTURE_PACKER_CWL_V_TRANSFORMATION_UBOS_1024};
     ShaderCache shader_cache(requested_shaders, sinks);
     Batcher batcher(shader_cache);
-    TexturePacker texture_packer(
-        "assets/packed_textures/packed_texture.json",
-        {"assets/packed_textures/packed_texture_0.png", "assets/packed_textures/packed_texture_1.png"});
+
+    const auto textures_directory = std::filesystem::path("assets");
+    std::filesystem::path output_dir = std::filesystem::path("assets") / "packed_textures";
+    int container_side_length = 1024;
+
+    TexturePacker texture_packer(textures_directory, output_dir, container_side_length);
+    shader_cache.set_uniform(ShaderType::TEXTURE_PACKER_CWL_V_TRANSFORMATION_UBOS_1024,
+                             ShaderUniformVariable::PACKED_TEXTURE_BOUNDING_BOXES, 1);
 
     FPSCamera camera(glm::vec3(0, 0, 3), 50, SCREEN_WIDTH, SCREEN_HEIGHT, 90, 0.1, 50);
     std::function<void(unsigned int)> char_callback = [](unsigned int _) {};
@@ -74,29 +79,26 @@ int main() {
     std::function<void(int, int, int)> mouse_button_callback = [](int _, int _1, int _2) {};
     GLFWLambdaCallbackManager glcm(window, char_callback, key_callback, mouse_pos_callback, mouse_button_callback);
 
-    std::vector<IVPTextured> lighter = parse_model_into_ivpts("assets/models/lighter.obj", true);
-    std::vector<IVPTexturePacked> packed_lighter = convert_ivpt_to_ivptp(lighter, texture_packer);
+    std::vector<draw_info::IVPTextured> lighter = model_loading::parse_model_into_ivpts(
+        "assets/models/lighter.obj",
+        batcher.texture_packer_cwl_v_transformation_ubos_1024_shader_batcher.object_id_generator, false);
+    std::vector<draw_info::IVPTexturePacked> packed_lighter =
+        texture_packer_model_loading::convert_ivpt_to_ivptp(lighter, texture_packer);
 
-    AnimatedTextureAtlas animated_texture_atlas("assets/spritesheets/flame.json", "assets/spritesheets/flame.png",
-                                                30.0);
+    AnimatedTextureAtlas animated_texture_atlas("assets/spritesheets/flame.json", "assets/spritesheets/flame.png", 30.0,
+                                                true, texture_packer);
 
-    GLuint ltw_matrices_gl_name;
-    glm::mat4 ltw_matrices[1024];
-
-    // initialize all matrices to identity matrices
-    for (int i = 0; i < 1024; ++i) {
-        ltw_matrices[i] = glm::mat4(1.0f);
-    }
-
-    glGenBuffers(1, &ltw_matrices_gl_name);
-    glBindBuffer(GL_UNIFORM_BUFFER, ltw_matrices_gl_name);
-    glBufferData(GL_UNIFORM_BUFFER, sizeof(ltw_matrices), ltw_matrices, GL_STATIC_DRAW);
-    glBindBufferBase(GL_UNIFORM_BUFFER, 0, ltw_matrices_gl_name);
-
-    std::vector<unsigned int> ltw_mat_idxs(4, 0);
-
-    auto pt_idx = texture_packer.get_packed_texture_index_of_texture("assets/spritesheets/flame.png");
-    std::vector<int> pt_idxs(4, pt_idx); // 4 because square
+    auto flame_st = texture_packer.get_packed_texture_sub_texture("assets/spritesheets/flame.png");
+    float flame_height = 4;
+    float flame_width = 1.7;
+    Transform flame;
+    flame.position = glm::vec3(0, flame_height / 2.0, 0);
+    auto dummy = vertex_geometry::generate_rectangle_texture_coordinates();
+    draw_info::IVPTexturePacked packed_flame(
+        vertex_geometry::generate_rectangle_indices(),
+        vertex_geometry::generate_rectangle_vertices(0, 0, flame_width, flame_height), dummy, dummy,
+        flame_st.packed_texture_index, flame_st.packed_texture_bounding_box_index, "",
+        batcher.texture_packer_cwl_v_transformation_ubos_1024_shader_batcher.object_id_generator.get_id());
 
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
@@ -113,7 +115,6 @@ int main() {
     std::vector<glm::vec2> packed_tex_coords_last_tick;
     int curr_obj_id = 0;
     while (!glfwWindowShouldClose(window)) {
-
         double current_time = glfwGetTime();
         double delta_time = current_time - previous_time;
         previous_time = current_time;
@@ -130,13 +131,13 @@ int main() {
         glm::mat4 projection = camera.get_projection_matrix();
         glm::mat4 view = camera.get_view_matrix();
 
-        unsigned int count = 0;
         for (auto &ivptp : packed_lighter) {
             std::vector<unsigned int> ltw_indices(ivptp.xyz_positions.size(), 0);
             std::vector<int> ptis(ivptp.xyz_positions.size(), ivptp.packed_texture_index);
+            std::vector<int> ptbbis(ivptp.xyz_positions.size(), ivptp.packed_texture_bounding_box_index);
             batcher.texture_packer_cwl_v_transformation_ubos_1024_shader_batcher.queue_draw(
-                count, ivptp.indices, ivptp.xyz_positions, ltw_indices, ptis, ivptp.packed_texture_coordinates);
-            count++;
+                ivptp.id, ivptp.indices, ltw_indices, ptis, ivptp.packed_texture_coordinates, ptbbis,
+                ivptp.xyz_positions);
         }
 
         shader_cache.set_uniform(ShaderType::TEXTURE_PACKER_CWL_V_TRANSFORMATION_UBOS_1024,
@@ -146,36 +147,34 @@ int main() {
 
         // use animated texture atlas
         // --------------------------
+        // TODO: was going to compare with the observatory to figure out why its upside down...
         double ms_curr_time = glfwGetTime() * 1000.0;
-        std::vector<glm::vec2> atlas_texture_coordinates =
-            animated_texture_atlas.get_texture_coordinates_of_sprite(ms_curr_time);
-        auto packed_tex_coords =
-            texture_packer.get_packed_texture_coordinates("assets/spritesheets/flame.png", atlas_texture_coordinates);
+        std::vector<glm::vec2> flame_ptcs =
+            animated_texture_atlas.get_texture_coordinates_of_current_animation_frame(ms_curr_time);
+
         bool new_coords = false;
-        if (packed_tex_coords != packed_tex_coords_last_tick) {
+        if (flame_ptcs != packed_tex_coords_last_tick) {
             new_coords = true;
             curr_obj_id += 1;
         }
-        packed_tex_coords_last_tick = packed_tex_coords;
+        packed_tex_coords_last_tick = flame_ptcs;
 
         glm::mat4 billboard_transform = compute_transform_to_rotate_basis_to_new_basis(
             camera.transform.compute_right_vector(), glm::vec3(0, 1, 0), camera.transform.compute_forward_vector());
 
-        ltw_matrices[animated_flame_ltw_mat_index] = flame.get_transform_matrix() * billboard_transform;
+        batcher.texture_packer_cwl_v_transformation_ubos_1024_shader_batcher
+            .ltw_matrices[animated_flame_ltw_mat_index] = flame.get_transform_matrix() * billboard_transform;
 
-        // load in the matrices
-        glBindBuffer(GL_UNIFORM_BUFFER, ltw_matrices_gl_name);
-        glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(ltw_matrices), ltw_matrices);
-        glBindBuffer(GL_UNIFORM_BUFFER, 0);
+        std::vector<unsigned int> flame_ltw_mat_idxs(4, animated_flame_ltw_mat_index);
+        std::vector<int> flame_ptis(4, packed_flame.packed_texture_index);
+        std::vector<int> flame_ptbbis(4, packed_flame.packed_texture_bounding_box_index);
 
-        // --------------------------
-        //
-        //
-        std::vector<unsigned int> flame_ltw_mat_idxs(4, 1);
-
+        // have to replace to put in the new texture coords
         batcher.texture_packer_cwl_v_transformation_ubos_1024_shader_batcher.queue_draw(
-            count + curr_obj_id, flame_indices, flame_vertices, flame_ltw_mat_idxs, pt_idxs, packed_tex_coords);
+            packed_flame.id, packed_flame.indices, flame_ltw_mat_idxs, flame_ptis, flame_ptcs, flame_ptbbis,
+            packed_flame.xyz_positions, new_coords);
 
+        batcher.texture_packer_cwl_v_transformation_ubos_1024_shader_batcher.upload_ltw_matrices();
         batcher.texture_packer_cwl_v_transformation_ubos_1024_shader_batcher.draw_everything();
 
         glfwSwapBuffers(window);
